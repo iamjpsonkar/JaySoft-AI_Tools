d1287733c638bfe605204c6f1e6fde471dc4cc5b
Jay Prakash Sonkar <iamjpsonkar@gmail.com>
2026-07-25 22:12:49 +0530

    chore: bump version to 0.2.0

--- jsat/__init__.py
+++ jsat/__init__.py
@@ -44,7 +44,7 @@ from jsat._exceptions import (
     UnsupportedLanguage,
 )
 
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 __author__ = "Jay Prakash Sonkar"
 __email__ = "iamjpsonkar@gmail.com"
 __license__ = "MIT"
